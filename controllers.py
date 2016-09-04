3a906066c72668c5041d1c7110865f41fc197b6e
madhephaestus <mad.hephaestus@gmail.com>
2016-09-04 19:51:46 -0400

    Stub module added

--- controllers.py
+++ controllers.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 from openerp import http
 
-class DefaultDataDemo(http.Controller):
-    @http.route('/physical_access_control/physical_access_control/', auth='public')
-    def index(self, **kw):
-        return "Hello, world"
-
-    @http.route('/physical_access_control/physical_access_control/objects/', auth='public')
-    def list(self, **kw):
-        return http.request.render('physical_access_control.listing', {
-            'root': '/physical_access_control/physical_access_control',
-            'objects': http.request.env['physical_access_control.physical_access_control'].search([]),
-        })
-
-    @http.route('/physical_access_control/physical_access_control/objects/<model("physical_access_control.physical_access_control"):obj>/', auth='public')
-    def object(self, obj, **kw):
-        return http.request.render('physical_access_control.object', {
-            'object': obj
-        })
+# class DefaultDataDemo(http.Controller):
+#     @http.route('/physical_access_control/physical_access_control/', auth='public')
+#     def index(self, **kw):
+#         return "Hello, world"
+# 
+#     @http.route('/physical_access_control/physical_access_control/objects/', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('physical_access_control.listing', {
+#             'root': '/physical_access_control/physical_access_control',
+#             'objects': http.request.env['physical_access_control.physical_access_control'].search([]),
+#         })
+# 
+#     @http.route('/physical_access_control/physical_access_control/objects/<model("physical_access_control.physical_access_control"):obj>/', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('physical_access_control.object', {
+#             'object': obj
+#         })
--- models.py
+++ models.py
@@ -4,6 +4,8 @@ from openerp import models, fields, api
 
 class physical_access_control(models.Model):
     _name = 'physical.access.control'
-    _description = 'Demo default data'
+    _description = 'Front Door'
     name = fields.Char('Name', required=True)
-    description = fields.Html('Description')
+    description = fields.Char('Device ID', required=True)
+    
+    
