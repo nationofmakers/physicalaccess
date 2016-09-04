83ad4946983411b7cd48eb8039e20d57c14a77ec
madhephaestus <mad.hephaestus@gmail.com>
2016-09-04 16:01:27 -0400

    first attempt to refactor

--- controllers.py
+++ controllers.py
@@ -2,19 +2,19 @@
 from openerp import http
 
 class DefaultDataDemo(http.Controller):
-    @http.route('/default_data_demo/default_data_demo/', auth='public')
+    @http.route('/physical_access_control/physical_access_control/', auth='public')
     def index(self, **kw):
         return "Hello, world"
 
-    @http.route('/default_data_demo/default_data_demo/objects/', auth='public')
+    @http.route('/physical_access_control/physical_access_control/objects/', auth='public')
     def list(self, **kw):
-        return http.request.render('default_data_demo.listing', {
-            'root': '/default_data_demo/default_data_demo',
-            'objects': http.request.env['default_data_demo.default_data_demo'].search([]),
+        return http.request.render('physical_access_control.listing', {
+            'root': '/physical_access_control/physical_access_control',
+            'objects': http.request.env['physical_access_control.physical_access_control'].search([]),
         })
 
-    @http.route('/default_data_demo/default_data_demo/objects/<model("default_data_demo.default_data_demo"):obj>/', auth='public')
+    @http.route('/physical_access_control/physical_access_control/objects/<model("physical_access_control.physical_access_control"):obj>/', auth='public')
     def object(self, obj, **kw):
-        return http.request.render('default_data_demo.object', {
+        return http.request.render('physical_access_control.object', {
             'object': obj
         })
--- models.py
+++ models.py
@@ -2,8 +2,8 @@
 
 from openerp import models, fields, api
 
-class default_data_demo(models.Model):
-    _name = 'demo.default.data'
+class physical_access_control(models.Model):
+    _name = 'physical.access.control'
     _description = 'Demo default data'
     name = fields.Char('Name', required=True)
     description = fields.Html('Description')
